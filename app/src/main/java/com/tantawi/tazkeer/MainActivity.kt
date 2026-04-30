5e24eb221fc7a0f408971b39cdfdb4fcd50c6620
Kareem <kareemelhosiney762@gmail.com>
2026-04-30 04:09:05 +0200

    Add Task(xml , kotlin)

--- app/src/main/java/com/tantawi/tazkeer/MainActivity.kt
+++ app/src/main/java/com/tantawi/tazkeer/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<Button>(R.id.btnShow).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, AddNewTask())
+                .addToBackStack(null)
+                .commit()
         }
     }
 }
